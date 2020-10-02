2d2966cb4b6cebc950e3ae2fd98791845fe5fd36
SweetSheldon <sheldongamesandlp@gmail.com>
2020-10-03 02:49:01 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    // var result = (n).toString().split("").reverse().join("");
+    // if(result[result.length-1]=='-'){return result.slice(0,-1)}
+    // else{return result}
+ return Math.abs(n).toString().split("").reverse().join("")
 }
